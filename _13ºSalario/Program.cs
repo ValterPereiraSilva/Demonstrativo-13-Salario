a5f6ce8204870a19b1f7cb37180d87777560820a
ValterSilva <valterpereira280667@gmail.com>
2019-09-14 18:47:54 -0300

    Atualizado 14/09/2019

--- _13ºSalario/Program.cs
+++ _13ºSalario/Program.cs
@@ -14,51 +14,58 @@ namespace _13ºSalario
 
         static void Main(string[] args)
         {
-            DecimoTerceiroSalario calculo = new DecimoTerceiroSalario();
+            try
+            {
+                DecimoTerceiroSalario calculo = new DecimoTerceiroSalario();
 
-            Console.Write("Digite o Nome do Funcionário: ");
-            string funcionario = Console.ReadLine();
-            Console.Write(funcionario + " Qual o Valor da Sua Hora: ");
-            double hora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write(funcionario + " Você tem Reflexos Sobre Décimo Terceiro (s/n)?: ");
-            char pergunta = char.Parse(Console.ReadLine());
-            if (pergunta == 's' || pergunta == 'S')
-            {
-                Console.Write(funcionario + " Você tem Hora Extra à 75% (s/n)?: ");
+                Console.Write("Digite o Nome do Funcionário: ");
+                string funcionario = Console.ReadLine();
+                Console.Write(funcionario + " Qual o Valor da Sua Hora: ");
+                double hora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write(funcionario + " Você tem Reflexos Sobre Décimo Terceiro (s/n)?: ");
+                char pergunta = char.Parse(Console.ReadLine());
+                if (pergunta == 's' || pergunta == 'S')
+                {
+                    Console.Write(funcionario + " Você tem Hora Extra à 75% (s/n)?: ");
+                    pergunta = char.Parse(Console.ReadLine());
+                    if (pergunta == 's' || pergunta == 'S')
+                    {
+                        Console.Write("Quantas Horas à 75% são: ");
+                        horaExtra75 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
+                    }
+                    Console.Write(funcionario + " Você tem Hora Extra à 100% (s/n)?: ");
+                    pergunta = char.Parse(Console.ReadLine());
+                    if (pergunta == 's' || pergunta == 'S')
+                    {
+                        Console.Write("Quantas Horas à 100% são: ");
+                        horaExtra100 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
+                    }
+                    Console.Write(funcionario + " Você tem Horas de Adicional Noturno (s/n)?: ");
+                    pergunta = char.Parse(Console.ReadLine());
+                    if (pergunta == 's' || pergunta == 'S')
+                    {
+                        Console.Write("Quantas Horas de Adicional Noturno são: ");
+                        adicionalNoturno = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
+                    }
+                }
+                Console.Write(funcionario + " Você tem Dependentes para Dedução do IRRF (s/n)?: ");
                 pergunta = char.Parse(Console.ReadLine());
                 if (pergunta == 's' || pergunta == 'S')
                 {
-                    Console.Write("Quantas Horas à 75% são: ");
-                    horaExtra75 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Quantos Dependentes são: ");
+                    dependente = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
                 }
-                Console.Write(funcionario + " Você tem Hora Extra à 100% (s/n)?: ");
-                pergunta = char.Parse(Console.ReadLine());
-                if (pergunta == 's' || pergunta == 'S')
-                {
-                    Console.Write("Quantas Horas à 100% são: ");
-                    horaExtra100 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
-                }
-                Console.Write(funcionario + " Você tem Horas de Adicional Noturno (s/n)?: ");
-                pergunta = char.Parse(Console.ReadLine());
-                if (pergunta == 's' || pergunta == 'S')
-                {
-                    Console.Write("Quantas Horas de Adicional Noturno são: ");
-                    adicionalNoturno = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
-                }
+                Console.WriteLine();
+                Console.WriteLine(calculo.ToString());
             }
-            Console.Write(funcionario + " Você tem Dependentes para Dedução do IRRF (s/n)?: ");
-            pergunta = char.Parse(Console.ReadLine());
-            if (pergunta == 's' || pergunta == 'S')
+            catch (Exception e)
             {
-                Console.Write("Quantos Dependentes são: ");
-                dependente = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                calculo = new DecimoTerceiroSalario(funcionario, hora, horaExtra75, horaExtra100, adicionalNoturno, dependente);
+                Console.WriteLine(e.Message);
             }
-            Console.WriteLine();
-            Console.WriteLine(calculo.ToString());
         }
     }
 }
